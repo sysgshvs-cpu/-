1d66ced9b211a27f5c6e0ae02196829b63ad95d1
sysgshvs-cpu <sysgshvs@gmail.com>
2026-07-15 05:30:41 +0300

    Delete app/src/main/models/SkinData.kt

--- app/src/main/models/SkinData.kt
+++
@@ -1,9 +0,0 @@
-package com.modark.pubgunlocker.models
-
-data class SkinData(
-    val id: String,
-    val name: String,
-    val category: String, // مثل: "weapons", "vehicles", "outfits"
-    val sourcePath: String, // مسار الملف المعدل داخل التطبيق
-    val targetPath: String  // المسار المستهدف داخل ملفات اللعبة (Android/data)
-)
